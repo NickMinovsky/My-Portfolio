0b27690a6893798c0920d5a46608fb8456e1110f
Nick <nickminovsky@gmail.com>
2019-03-28 17:53:37 +0200

    second version

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 $(document).ready(function() {
-  $(".menu-toggler").on("click", function() {
-    $(this).toggleClass("open");
-    $(".top-nav").toggleClass("open");
+  $(".up").on("click", function() {
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+    return false;
+  });
+
+  AOS.init({
+    easing: "ease",
+    duration: 1500,
+    once: true
+  });
+
+  $(function() {
+    $("#emailme").click(function(event) {
+      var email = "NickMinovsky@gmail.com";
+      var subject = "";
+      var emailBody = "Your message...";
+      var attach = "path";
+      document.location =
+        "mailto:" + email + "?subject=" + subject + "&body=" + emailBody;
+    });
+  });
+
+  $(".nav-item").on("click", event => {
+    $(event.currentTarget)
+      .children()
+      .css("opacity", 1);
+    $(event.currentTarget)
+      .siblings()
+      .children()
+      .css("opacity", 0.4);
   });
 });
